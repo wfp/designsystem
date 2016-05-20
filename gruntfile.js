1d1f17ed8e1f294d705c0c880afaa8b94045c175
Matthew Morek <me@matthewmorek.com>
2016-05-20 10:59:42 +0100

    Update `eslint` configuration
    - [x] Ignore linting for external libs, under `js/lib` path

--- gruntfile.js
+++ gruntfile.js
@@ -233,11 +233,10 @@ module.exports = function(grunt) {
       }
     },
     eslint: {
+      target: ['js/**/*.js'],
       options: {
-        config: '.eslintrc.json',
-        force: true
-      },
-      all: ['js/**/*.js']
+        ignorePattern: ['js/lib/**/*.js']
+      }
     },
     sasslint: {
       options: {
@@ -276,15 +275,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-datauri');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-sass-lint');
   grunt.loadNpmTasks('grunt-svg-toolkit');
   grunt.loadNpmTasks('grunt-pure-grids');
-  grunt.loadNpmTasks('eslint-grunt');
 
   // Build SVGs and SCSS
   grunt.registerTask('gen-svg', ['clean:svg', 'svgtoolkit', 'datauri', 'clean:svg']);
