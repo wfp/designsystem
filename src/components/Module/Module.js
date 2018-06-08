052bb15f47edc5a5e6587eeb5caec5c77e08cd97
Robert Gühne <robert@polyxostudios.de>
2018-06-08 07:59:26 +0200

    Minor fixes

--- src/components/Module/Module.js
+++ src/components/Module/Module.js
@@ -66,8 +66,7 @@ const ModuleFooter = ({ children, className, ...other }) => {
   const wrapperClasses = classNames('wfp--module__footer', className);
 
   return (
-    <div className={wrapperCl
-      asses} {...other}>
+    <div className={wrapperClasses} {...other}>
       {children}
     </div>
   );
