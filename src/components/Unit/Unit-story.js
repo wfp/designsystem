9cde18dd0e977f7a20dcc7d5360c73a72042ecf8
Robert Gühne <robert@polyxostudios.de>
2018-06-16 17:02:36 +0200

    Table cell stylings added

--- src/components/Unit/Unit-story.js
+++ src/components/Unit/Unit-story.js
@@ -13,9 +13,145 @@ storiesOf('Unit', module)
       ~~~js
       import { Unit } from '@wfp/ui';
       ~~~
+
+
+      List of available Units
+
+      Usd: Generate Million USD based on USD 73821341 => 73.82 M USD 
+      Tusd,
+      Musd,
+      Busd: Generate Billion USD based on USD 7331821341 => 73.82 B USD 
+      Littleusd,
+      Partners,
+      Beneficiaries: 133212 => 133.212 Beneficiaries
+      Households: 133212 => 133.212 Households
+      Months,
+      Mt: Format MT 50 => 50 MT
+      Littlepeople,
+      People,
+      Mpeople,
+      Million,
+      Percentage: Percentage 50 / from={ 100 } => 50%
+      SimplePercentage: 32.12 => 32.12 %
+      MonthYear: YYYYMM / from 201709  => Sep 2017
+      Year,
+      None: Don't do anything 32.12 => 32.12
+      NoneSvg,
+      Trips: 213123 => 213.123 Trips
+      Level: Format Level from 3 => Level 3
+      LevelSvg,
+      Countries,
+      UsdSvg,
+      TusdSvg,
+      MusdSvg,
+      PeopleSvg,
+      MonthYearSvg,
+      PercentageSvg,
+      MtSvg,
+      MusdCalc,
+      UsdCalc,
+      PercentageCalc,
+      SimplePercentageCalc
+
+
     `,
     () => <Unit
             type="Musd"
           >231321133</Unit>
+  )
+  .addWithInfo(
+    'available Units',
+    `
+      The example below shows an Unit.
+
+      ~~~js
+      import { Unit } from '@wfp/ui';
+      ~~~
+
+
+      List of available Units
+
+      Usd: Generate Million USD based on USD 73821341 => 73.82 M USD 
+      Tusd,
+      Musd,
+      Busd: Generate Billion USD based on USD 7331821341 => 73.82 B USD 
+      Littleusd,
+      Partners,
+      Beneficiaries: 133212 => 133.212 Beneficiaries
+      Households: 133212 => 133.212 Households
+      Months,
+      Mt: Format MT 50 => 50 MT
+      Littlepeople,
+      People,
+      Mpeople,
+      Million,
+      Percentage: Percentage 50 / from={ 100 } => 50%
+      SimplePercentage: 32.12 => 32.12 %
+      MonthYear: YYYYMM / from 201709  => Sep 2017
+      Year,
+      None: Don't do anything 32.12 => 32.12
+      NoneSvg,
+      Trips: 213123 => 213.123 Trips
+      Level: Format Level from 3 => Level 3
+      LevelSvg,
+      Countries,
+      UsdSvg,
+      TusdSvg,
+      MusdSvg,
+      PeopleSvg,
+      MonthYearSvg,
+      PercentageSvg,
+      MtSvg,
+      MusdCalc,
+      UsdCalc,
+      PercentageCalc,
+      SimplePercentageCalc
+
+
+    `,
+    () => <div>
+
+          
+          <table>
+            <thead>
+              <tr>
+              <td>Preview</td>
+              <td>type</td>
+              <td>additional</td>
+              </tr>
+            </thead>
+            <tr>
+              <td>
+                <Unit
+                  type="Usd"
+                >
+                  231321133
+                </Unit>
+              </td>
+              <td>
+                Usd
+              </td>
+              <td>
+                –
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Unit
+                  type="Usd"
+                >
+                  231321133
+                </Unit>
+              </td>
+              <td>
+                Usd
+              </td>
+              <td>
+                –
+              </td>
+            </tr>
+          </table>
+         
+          </div>
   )
 ;
