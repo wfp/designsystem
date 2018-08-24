0427f78a4dac9c5483c267ef770ced181a242b74
Robert Gühne <robert@polyxostudios.de>
2018-08-24 10:52:21 +0200

    WFP Sketch Template updated

--- src/documentation/Naming-story.js
+++ src/documentation/Naming-story.js
@@ -14,21 +14,26 @@ storiesOf(' Documentation', module)
       subTitle="Introduction to the new WFP UI Kit">
       <p>
         The UI Kit uses the{' '}
-        <a href="http://getbem.com/naming">BEM naming convention</a> with a{' '}
-        <i className="wfp--inline-highlight">wfp--</i> prefix as standard naming
-        for all css elements.
-      </p>
-      <p>
-        Please use ES-Lint which is a linting utility for JavaScript and JSX
-        while building your React Application to analyze source code to flag
-        programming errors, bugs, stylistic errors and suspicious constructs.
+        <a href="http://getbem.com/naming">BEM naming convention</a>.
       </p>
 
-      <ul>
-        <li>ECMAScript 6 features</li>
-        <li>no require statements</li>
-        <li>spaces (2 space indentation)</li>
-        <li>Semicolon is required at the end of a line</li>
-      </ul>
+    <h3>Namespacing</h3>
+    <p>
+      The Components are built to be included individually and not clobber global styles - all class attributes are prefixed by the <i className="wfp--inline-highlight">wfp--</i> moniker.
+    </p>
+
+    <h3>Linting</h3>
+    <p>
+      Please use ES-Lint which is a linting utility for JavaScript and JSX
+      while building your React Application to analyze source code to flag
+      programming errors, bugs, stylistic errors and suspicious constructs.
+    </p>
+
+    <ul className="wfp--story--list">
+      <li>ECMAScript 6 features</li>
+      <li>no require statements</li>
+      <li>spaces (2 space indentation)</li>
+      <li>Semicolon is required at the end of a line</li>
+    </ul>
     </Page>
   ));
