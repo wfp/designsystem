736400b048134f8c809bc91d9145dc876441a610
Robert Gühne <robert@polyxostudios.de>
2018-10-05 12:29:34 +0200

    Jest tests for redux-form wip

---
+++ src/internal/RfFormJest.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { Values } from 'redux-form-website-template';
+import Button from '../components/Button';
+import Blockquote from '../components/Blockquote';
+import FormItem from '../components/FormItem/RfFormItem';
+import FormLabel from '../components/FormLabel/RfFormLabel';
+import { load as loadAccount } from './loadDefaultData';
+
+class FormEl extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      handleSubmit,
+      pristine,
+      reset,
+      sampleData,
+      submitting,
+    } = this.props;
+
+    const data = sampleData
+      ? sampleData
+      : {
+          // used to populate "account" reducer when "Load" is clicked
+          Input: 13,
+        };
+
+    return (
+      <form onSubmit={handleSubmit} className="wfp-form--stacked">
+        {this.props.children}
+      </form>
+    );
+  }
+}
+
+let Form = reduxForm({
+  form: 'SimpleForm', //Form name is same
+  enableReinitialize: true,
+})(FormEl);
+
+// You have to connect() to any reducers that you wish to connect to yourself
+Form = connect(
+  state => ({
+    initialValues: state.account.data, // pull initial values from account reducer
+  }),
+  { load: loadAccount } // bind account loading action creator
+)(Form);
+
+class RfFormWrapper extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit(submit) {
+    alert('Submitted: ' + JSON.stringify(submit));
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.onSubmit} {...this.props}>
+        {this.props.children}
+      </Form>
+    );
+  }
+}
+
+export default RfFormWrapper;
