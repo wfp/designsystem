913e319adf61d7089318013462db5a934ef9b340
Robert Gühne <robert@polyxostudios.de>
2018-10-16 23:52:51 +0200

    Cdn Logo for Footer, SubNavigation for Js added

--- src/components/MainNavigation/main-navigation.js
+++ src/components/MainNavigation/main-navigation.js
@@ -1,3 +1,5 @@
+/* Navigation toggle on mobile */
+
 (function() {
 
   var hamburger = {
@@ -11,5 +13,59 @@
   };
 
   hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+}());
+
+/* Mobile Navigation activator */
+
+
+(function() {
+
+  var open = false;
+  var documentEvent = document;
+
+  const outsideClickListener = (event) => {
+    const element = document.querySelector('.wfp--main-navigation__sub--open')
+    if (!element.contains(event.target)) {
+        subItem.doToggle(event, undefined);
+    }
+  }
+
+  var subItem = {
+    navToggle: document.querySelectorAll('.wfp--main-navigation__trigger--has-sub'),
+    nav: document.querySelectorAll('.wfp--main-navigation__item'),
+    sub: document.querySelectorAll('.wfp--main-navigation__sub'),
+
+    doToggle: function(e, b) {
+      e.preventDefault();
+      if (documentEvent !== undefined)
+        documentEvent.removeEventListener('click', outsideClickListener, false);
+
+      for (let d = 0; d < subItem.navToggle.length; d++) {
+        this.navToggle[d].classList.remove('wfp--main-navigation__trigger--open');
+        this.nav[d].classList.remove('wfp--main-navigation__item--open');
+        this.sub[d].classList.remove('wfp--main-navigation__sub--open');
+      }
+      if (b !== open && b !== undefined) {
+        this.navToggle[b].classList.toggle('wfp--main-navigation__trigger--open');
+        this.nav[b].classList.toggle('wfp--main-navigation__item--open');
+        this.sub[b].classList.toggle('wfp--main-navigation__sub--open');
+        setTimeout(function(){
+          documentEvent.addEventListener('click', outsideClickListener, false);
+        }, 500);
+        open = b;
+      }
+      else {
+        open = false;
+      }
+    }
+  };
+
+  for (var n = 0; n < subItem.navToggle.length; n++) {
+    const u = n;
+    subItem.navToggle[n].addEventListener('click', function(e) {
+      subItem.doToggle(e, u);
+    });
+  }
 
 }());
