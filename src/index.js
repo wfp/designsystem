08316017fa4656c6e797904cdcf6de77a9bbf177
Robert Gühne <robert@polyxostudios.de>
2019-04-23 11:35:31 +0200

    feat(colors): Color json and export

--- src/index.js
+++ src/index.js
@@ -8,18 +8,14 @@ export Breadcrumb from './components/Breadcrumb';
 export BreadcrumbItem from './components/BreadcrumbItem';
 export BreadcrumbHome from './components/BreadcrumbHome';
 export Button from './components/Button';
-
 export Card from './components/Card';
 export Checkbox from './components/Checkbox';
 export Credits from './components/Credits';
-
 export ContentSwitcher from './components/ContentSwitcher';
-
 export {
   SingleDatePickerInput,
   DateRangePickerInput,
 } from './components/DatePicker';
-
 export FileUploader from './components/FileUploader';
 export Footer from './components/Footer';
 export Form from './components/Form';
@@ -29,18 +25,13 @@ export FormHint from './components/FormHint';
 export FormItem from './components/FormItem';
 export FormLabel from './components/FormLabel';
 export FormWizard from './components/FormWizard';
-
 export Icon from './components/Icon';
-
 export Link from './components/Link';
 export Loading from './components/Loading';
 export InlineLoading from './components/InlineLoading';
-
 export { List, ListItem } from './components/List';
-
 export MainNavigation from './components/MainNavigation';
 export MainNavigationItem from './components/MainNavigationItem';
-
 export {
   SubNavigation,
   SubNavigationHeader,
@@ -52,7 +43,6 @@ export {
   SubNavigationGroup,
   SubNavigationItem,
 } from './components/SubNavigation';
-
 export Modal from './components/Modal';
 export ModalWrapper from './components/ModalWrapper';
 export {
@@ -61,27 +51,20 @@ export {
   ModuleHeader,
   ModuleFooter,
 } from './components/Module';
-
 export notificationStyle from './components/Notification';
 export NumberInput from './components/NumberInput';
 export Pagination from './components/Pagination';
-
 export RadioButton from './components/RadioButton';
 export RadioButtonGroup from './components/RadioButtonGroup';
-
 export ReadMore from './components/ReadMore';
 export ReduxFormWrapper from './components/ReduxFormWrapper';
-
 export Search from './components/Search';
-
 export {
   SecondaryNavigation,
   SecondaryNavigationTitle,
 } from './components/SecondaryNavigation';
-
 export StepNavigation from './components/StepNavigation';
 export StepNavigationItem from './components/StepNavigationItem';
-
 export Select from './components/Select';
 export SelectItem from './components/SelectItem';
 export SelectItemGroup from './components/SelectItemGroup';
@@ -100,21 +83,18 @@ export {
 */
 
 export TablePagination from './components/TablePagination';
-
 export Tab from './components/Tab';
 export TabContent from './components/TabContent';
 export Tabs from './components/Tabs';
 export Tag from './components/Tag';
 export TextArea from './components/TextArea';
 export TextInput from './components/TextInput';
-
 export tooltipStyle from './components/Tooltip';
 export Toggle from './components/Toggle';
 /*
 export ToggleSmall from './components/ToggleSmall';
 */
 export User from './components/User';
-
 export Unit from './components/Unit';
-
 export Wrapper from './components/Wrapper';
+export colors from './globals/data/colors';
