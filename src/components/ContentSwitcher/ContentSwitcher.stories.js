3964d156ad80366a4284f47ae374ba579b8d2e12
cassandra <cassandra.kesewa@gmail.com>
2020-09-15 09:08:19 +0000

    refactor(ContentSwitcher): added switch subcomponent to contentSwitcher with examples

--- src/components/ContentSwitcher/ContentSwitcher.stories.js
+++ src/components/ContentSwitcher/ContentSwitcher.stories.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ContentSwitcher from './ContentSwitcher';
 import markdown from './README.mdx';
 import Switch from '../Switch';
+import { iconAddGlyph } from '@wfp/icons';
+import Icon from '../Icon';
 
 export default {
   title: 'Components/ContentSwitcher',
   component: ContentSwitcher,
+  subcomponents: { Switch },
   parameters: {
     componentSubtitle: 'Component',
     status: 'released',
@@ -15,7 +18,7 @@ export default {
 
 export const Regular = (args) => {
   const handleChange = () => {
-    console.log('handle any onChange here');
+    // console.log('handle any onChange here');
   };
   return (
     <ContentSwitcher {...args} onChange={handleChange}>
@@ -28,4 +31,81 @@ export const Regular = (args) => {
 
 Regular.args = {
   children: 'ContentSwitcher',
+};
+
+export const ButtonSwitch = (args) => {
+  const handleChange = () => {
+    // console.log('handle any onChange here');
+  };
+  return (
+    <ContentSwitcher {...args} onChange={handleChange}>
+      <Switch kind="button" name="one" text="First section" />
+      <Switch kind="button" name="two" text="Second section" />
+      <Switch kind="button" name="three" text="Third section" />
+    </ContentSwitcher>
+  );
+};
+
+ButtonSwitch.args = {
+  children: 'ContentSwitcher',
+};
+
+const description = `
+
+#### Default types of switch state
+
+| kind   |  Description  |
+|----------|-------------|
+| \`anchor\` | (Default) |
+| \`button\` | experimental   |
+`;
+
+ButtonSwitch.story = {
+  parameters: {
+    docs: {
+      storyDescription: description,
+    },
+  },
+};
+
+export const withIcon = (args) => {
+  const handleChange = () => {
+    // console.log('handle any onChange here');
+  };
+  return (
+    <ContentSwitcher {...args} onChange={handleChange}>
+      <Switch
+        kind="button"
+        name="one"
+        text="First section"
+        icon={<Icon icon={iconAddGlyph} />}
+      />
+      <Switch
+        kind="button"
+        name="two"
+        text="Second section"
+        icon={<Icon icon={iconAddGlyph} />}
+      />
+      <Switch
+        kind="button"
+        name="three"
+        text="Third section"
+        icon={<Icon icon={iconAddGlyph} />}
+      />
+    </ContentSwitcher>
+  );
+};
+
+withIcon.args = {
+  children: 'ContentSwitcher',
+};
+
+const desc = `Specify icon to be used in the switch element. \`icon\` property on \`<switch/>\` expects an element `;
+
+withIcon.story = {
+  parameters: {
+    docs: {
+      storyDescription: desc,
+    },
+  },
 };
