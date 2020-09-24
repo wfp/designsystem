90792bb946dd0517f2f6ef283732c4a3bf047157
cassandra <cassandra.kesewa@gmail.com>
2020-09-24 09:31:12 +0000

    refactor(ReactSelect): react select moved to new environment

---
+++ src/components/ReactSelect/ReactSelect.stories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactSelect from 'react-select';
+
+import markdown from './README.mdx';
+
+export default {
+  title: 'Forms/ReactSelect',
+  component: ReactSelect,
+  markdown: 'hello',
+  parameters: {
+    componentSubtitle: 'Component',
+    status: 'released',
+    // mdx: markdown,
+  },
+};
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+export const Default = (args) => (
+  <div className="wfp--form-item" style={{ minWidth: '400px' }}>
+    <label htmlFor="abc" className="wfp--label">
+      The Label
+    </label>
+    <ReactSelect
+      className="wfp--react-select-container"
+      classNamePrefix="wfp--react-select"
+      isMulti
+      id="abc"
+      options={options}
+      {...args}
+    />
+  </div>
+);
+Default.args = {
+  placeholder: 'Placeholder',
+  isSearch: true,
+  isMulti: true,
+  isDisabled: false,
+};
+
+Default.story = {
+  name: 'Default',
+  parameters: {
+    info: {
+      source: false,
+      propTables: false,
+    },
+  },
+};
