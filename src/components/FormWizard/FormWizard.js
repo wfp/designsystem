f7dc6e791c23c7bf7539c0a88c119fda2b2bb1e2
cassandra <cassandra.kesewa@gmail.com>
2020-09-24 10:09:24 +0000

    refactor(FormWizard): port formwizard to function component

--- src/components/FormWizard/FormWizard.js
+++ src/components/FormWizard/FormWizard.js
@@ -6,58 +6,56 @@ import { Module, ModuleBody, ModuleHeader, ModuleFooter } from '../Module';
 
 /** The FormWizard is a wrapper for creating Wizard forms. */
 
-export default class FormWizard extends React.Component {
-  static propTypes = {
-    /**
-     * Specify a custom css class
-     * which is added to the container
-     */
-    className: PropTypes.string,
-    /**
-     * Specify the custom
-     * main content component
-     */
-    children: PropTypes.node.isRequired,
-    /**
-     * Specify the custom
-     * sidebar component
-     */
-    sidebar: PropTypes.node.isRequired,
-    /**
-     * Sticky sidebar
-     */
-    stickySidebar: PropTypes.bool,
+const FormWizard = ({
+  className,
+  children,
+  formHeader,
+  formControls,
+  sidebar,
+  stickySidebar,
+}) => {
+  const classes = {
+    formControls: classNames(
+      'wfp--form-wizard',
+      { 'wfp--form-wizard--sticky': stickySidebar },
+      className
+    ),
   };
 
-  static defaultProps = {};
+  return (
+    <div className={classes.formControls}>
+      <div className="wfp--form-wizard__sidebar">{sidebar}</div>
+      <Module className="wfp--form-wizard__content" noMargin>
+        {formHeader && <ModuleHeader>{formHeader}</ModuleHeader>}
+        <ModuleBody>{children}</ModuleBody>
+        {formControls && <ModuleFooter>{formControls}</ModuleFooter>}
+      </Module>
+    </div>
+  );
+};
 
-  render() {
-    const {
-      className,
-      children,
-      formHeader,
-      formControls,
-      sidebar,
-      stickySidebar,
-    } = this.props;
+FormWizard.propTypes = {
+  /**
+   * Specify a custom css class
+   * which is added to the container
+   */
+  className: PropTypes.string,
+  /**
+   * Specify the custom
+   * main content component
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   * Specify the custom
+   * sidebar component
+   */
+  sidebar: PropTypes.node.isRequired,
+  /**
+   * Sticky sidebar
+   */
+  stickySidebar: PropTypes.bool,
+};
 
-    const classes = {
-      formControls: classNames(
-        'wfp--form-wizard',
-        { 'wfp--form-wizard--sticky': stickySidebar },
-        className
-      ),
-    };
+FormWizard.defaultProps = {};
 
-    return (
-      <div className={classes.formControls}>
-        <div className="wfp--form-wizard__sidebar">{sidebar}</div>
-        <Module className="wfp--form-wizard__content" noMargin>
-          {formHeader && <ModuleHeader>{formHeader}</ModuleHeader>}
-          <ModuleBody>{children}</ModuleBody>
-          {formControls && <ModuleFooter>{formControls}</ModuleFooter>}
-        </Module>
-      </div>
-    );
-  }
-}
+export default FormWizard;
