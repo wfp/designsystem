6b4d935880e6903138312d4e46f570e1a73dcba7
Laura Silvani <laura.silvani@wfp.org>
2020-11-06 09:39:13 +0100

    Updated docs on usage, updated order of sections under the Getting started section

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -95,7 +95,7 @@ export const parameters = {
     storySort: {
       order: [
         'Getting started',
-        ['Intro', 'Browser support', 'Designers'],
+          ['Intro', 'Installing the Kit','Using the Kit','Recommended libraries', 'Browser support', 'Design resources','Support and Feedback','Credits'],
         'Documentation',
         'Templates',
         'Components',
