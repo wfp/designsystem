6b1297e23f493d54d571d2435199422b175f872d
cassandra <cassandra.kesewa@gmail.com>
2021-01-20 08:28:50 +0000

    docs(TableWithFilter): added table with filter template, in progress

---
+++ src/documentation/TableWithFilter/Form.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import { useForm, Controller } from 'react-hook-form';
+import FormGroup from '../../components/FormGroup';
+import TextInput from '../../components/TextInput';
+import NumberInput from '../../components/NumberInput';
+import Button from '../../components/Button';
+import ReactSelect from 'react-select';
+
+const options = [
+    { value: 'male', label: 'Male' },
+    { value: 'female', label: 'Female' },
+  ];
+const countryOptions = [
+    
+    { value: 'armenia', label: 'Armenia' },
+    { value: 'brazil', label: 'Brazil' },
+    { value: 'china', label: 'China' },
+    { value: 'france', label: 'France' },
+    { value: 'honduras', label: 'Honduras' },
+    { value: 'indonesia', label: 'Indonesia' },
+    { value: 'morocco', label: 'Morocco' },
+    { value: 'pakistan', label: 'Pakistan' },
+    { value: 'russia', label: 'Russia' },
+    { value: 'zimbabwe', label: 'Zimbabwe' },
+    
+]
+
+function Form() {
+    const { handleSubmit, register, errors, control } = useForm();
+
+    const handleFilter = (formdata) => {
+        console.log("data: ", formdata);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleFilter)}>
+            <FormGroup className="wfp--form-long"
+                align="horizontal"
+                style={{ marginTop: '1rem' }}>
+                <div className="wfp--form-item" style={{ minWidth: '100px' }}>
+                <label htmlFor="country" className="wfp--label">
+                    Gender
+                </label>
+                <Controller
+                    as={ReactSelect}
+                    name="gender"
+                    className="wfp--react-select-container"
+                    classNamePrefix="wfp--react-select"
+                    control={control}
+                    onChange={([selected]) => {
+                    return { selected };
+                    }}
+                    options={options}
+                    defaultValue={{}}
+                />
+                {/* <ReactSelect
+                    className="wfp--react-select-container"
+                    classNamePrefix="wfp--react-select"
+                    id="gender"
+                    name="gender"
+                    options={options}
+                    ref={register}
+                /> */}
+                </div>
+                <div className="wfp--form-item" style={{ minWidth: '100px' }}>
+                <label htmlFor="city" className="wfp--label">
+                    Country
+                </label>
+                <ReactSelect
+                    className="wfp--react-select-container"
+                    classNamePrefix="wfp--react-select"
+                    id="country"
+                    name="country"
+                    // onChange={handleCityChange}
+                    options={countryOptions}
+                    ref={register}
+                />
+                </div>
+                
+            </FormGroup>
+            <FormGroup className="wfp--form-long" align="horizontal">
+                <TextInput
+                id="firstname"
+                name="firstname"
+                labelText="Firstname"
+                placeholder="eg: Edith"
+                inputRef={register}
+                />
+                <TextInput
+                id="street"
+                name="street"
+                labelText="Street"
+                placeholder="eg: Chemin Aime Steinlein"
+                inputRef={register}
+                />
+                <NumberInput id="age" name="age" labelText="Age" placeholder="" inputRef={register} />
+            </FormGroup>
+
+            <div
+            className="buttons"
+            style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginTop: '1em',
+            }}>
+            <Button
+                type="button"
+                kind="ghost"
+                style={{ marginRight: '0.5em' }}>
+                Clear filters
+            </Button>
+            <Button type="submit">Apply</Button>
+            </div>
+        </form>
+    )
+}
+
+export default Form
---
+++ src/documentation/TableWithFilter/TableWithFilter.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react'
+
+import Search from '../../components/Search';
+import Button from '../../components/Button';
+
+import { iconFilterAlt, iconSubtractGlyph } from '@wfp/icons';
+
+import { Module, ModuleHeader, ModuleBody } from '../../components/Module';
+import Table from './DataTable'
+import MyForm from './Form';
+
+function TableWithFilter() {
+    const [simpleSearch, setSimpleSearch] = useState();
+    const [filterActivated, setFilterActivated] = useState(false);
+    const [search, setSearch] = useState({})
+
+    const handleSearchOnchange = (value) =>{
+        setSimpleSearch(value)
+    }
+
+    const toggleFilter = () => {
+        if (filterActivated) {
+          setSearch({})
+        }
+        setFilterActivated(!filterActivated);
+    };
+
+    return (
+        <Module noMargin>
+        <ModuleHeader
+          filter={
+            <>
+              <Search
+                id="tablewithfilter"
+                placeHolderText="Type to filter"
+                value={simpleSearch}
+                onChange={handleSearchOnchange}
+              />
+              <Button
+                onClick={toggleFilter}
+                icon={filterActivated ? iconSubtractGlyph : iconFilterAlt}
+                kind="secondary"
+                style={{ marginLeft: '1em' }}>
+                {filterActivated ? 'Hide filters' : 'Advanced filters'}
+              </Button>
+            </>
+          }>
+          Large table with filter
+        </ModuleHeader>
+        {filterActivated && (
+          <ModuleBody>
+            <MyForm />
+          </ModuleBody>
+        )}
+        <Table filterText={simpleSearch}/> 
+      </Module>
+    )
+}
+
+export default TableWithFilter
