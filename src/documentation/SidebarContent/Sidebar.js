52cae4fff71bde792f5fae1a0b1a40206b61c5c6
cassandra <cassandra.kesewa@gmail.com>
2021-01-29 09:36:21 +0000

    docs(sidebar): sidebar component in progress

--- src/documentation/SidebarContent/Sidebar.js
+++ src/documentation/SidebarContent/Sidebar.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 import SidebarItem from '../../components/SidebarItem/SidebarItem';
 import Search from '../../components/Search';
 
-function Sidebar({data, content, sidebarItemSchema, updateContent, enableSearch, ...other}) {
+function Sidebar({
+    data, 
+    content, 
+    sidebarItemSchema, 
+    updateContent, 
+    enableSearch, 
+    ...other}) {
    
     return (
-        <div className="wfp--sidebar-content__container">
+        <div className="wfp--sidebar-content__container" {...other}>
             <div className="wfp--sidebar-item-content" >
                 {
                    enableSearch &&  (
