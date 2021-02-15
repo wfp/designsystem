3b4c470b7bbe66b29fb4c7f490988a62cc25dd77
cassandra <cassandra.kesewa@gmail.com>
2021-02-15 14:49:36 +0000

    docs(sidebar): sidebar export resolved

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,1 +1,4 @@
-export { Sidebar, SidebarHeader, SidebarScroll, SidebarBackButton } from './Sidebar';
+import Sidebar from './Sidebar';
+
+export default from './Sidebar';
+export { SidebarHeader, SidebarScroll, SidebarBackButton } from './Sidebar';
