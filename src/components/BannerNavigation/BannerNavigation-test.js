710ae0468830b02f9eca08e9fb0e30eea426241e
cassandra <cassandra.kesewa@gmail.com>
2021-03-04 17:01:10 +0000

    test(BannerNavigation): test cases for BannerNavigationa and InfoBar

--- src/components/BannerNavigation/BannerNavigation-test.js
+++ src/components/BannerNavigation/BannerNavigation-test.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import BannerNavigation from './BannerNavigation';
+import {BannerNavigation, BannerNavigationItem} from '../BannerNavigation';
 import { shallow } from 'enzyme';
 
 describe('BannerNavigation', ()=>{
 
     describe('renders as expected', ()=>{
-        const wrapper = shallow(<BannerNavigation />);
+        const wrapper = shallow(<BannerNavigation>Items placed here</BannerNavigation>);
 
         const bannerWrapper = () => wrapper.find('div.wfp--banner-navigation'); 
 
         describe('bannerWrapper', () => {
             it('renders as expected', () => {
                 expect(bannerWrapper().length).toBe(1);
-            })
+            });
+
+            it('renders default classes as expected', () => {
+                expect(bannerWrapper().hasClass('wfp--banner-navigation')).toEqual(true);
+            });
+
+            it('renders unoredered list as expected', () => {
+                expect(bannerWrapper().find('ul').length).toBe(1);
+            });
+
+            
         })
+    })
+})
+
+
+describe('BannerNavigationItem', ()=>{
+
+    describe('renders as expected', ()=>{
+        const wrapper = shallow(<BannerNavigationItem>Items placed here</BannerNavigationItem>);
+
+            it('renders as expected', () => {
+                expect(wrapper.length).toBe(1);
+            });
+
+            it('renders default classes as expected', () => {
+                expect(wrapper.hasClass('wfp--banner-navigation__item')).toEqual(true);
+            });
+
+            it('renders unoredered list as expected', () => {
+                expect(wrapper.find('li').length).toBe(1);
+            });
     })
 })
