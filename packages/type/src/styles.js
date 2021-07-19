64eb830c5e328e5e34bfad30ee2799f0ceca81c3
cassandra <cassandra.kesewa@gmail.com>
2021-07-19 14:08:46 +0000

    updated and customized type

--- packages/type/src/styles.js
+++ packages/type/src/styles.js
@@ -10,12 +10,12 @@ import { fontWeights } from './fontWeight';
 import { fontFamilies } from './fontFamily';
 import { scale } from './scale';
 
-export const caption01 = {
-  fontSize: rem(scale[0]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.34,
-  letterSpacing: px(0.32),
-};
+// export const caption01 = {
+//   fontSize: rem(scale[0]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.34,
+//   letterSpacing: px(0.32),
+// };
 
 export const caption02 = {
   fontSize: rem(scale[1]),
@@ -24,12 +24,12 @@ export const caption02 = {
   letterSpacing: px(0.32),
 };
 
-export const label01 = {
-  fontSize: rem(scale[0]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.34,
-  letterSpacing: px(0.32),
-};
+// export const label01 = {
+//   fontSize: rem(scale[0]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.34,
+//   letterSpacing: px(0.32),
+// };
 
 export const label02 = {
   fontSize: rem(scale[1]),
@@ -44,32 +44,32 @@ export const helperText01 = {
   letterSpacing: px(0.32),
 };
 
-export const helperText02 = {
-  fontSize: rem(scale[1]),
-  lineHeight: 1.29,
-  letterSpacing: px(0.32),
-};
+// export const helperText02 = {
+//   fontSize: rem(scale[1]),
+//   lineHeight: 1.29,
+//   letterSpacing: px(0.32),
+// };
 
-export const bodyShort01 = {
-  fontSize: rem(scale[1]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.29,
-  letterSpacing: px(0.16),
-};
+// export const bodyShort01 = {
+//   fontSize: rem(scale[1]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.29,
+//   letterSpacing: px(0.16),
+// };
 
-export const bodyLong01 = {
-  fontSize: rem(scale[1]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.43,
-  letterSpacing: px(0.16),
-};
+// export const bodyLong01 = {
+//   fontSize: rem(scale[1]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.43,
+//   letterSpacing: px(0.16),
+// };
 
-export const bodyShort02 = {
-  fontSize: rem(scale[2]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.375,
-  letterSpacing: 0,
-};
+// export const bodyShort02 = {
+//   fontSize: rem(scale[2]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.375,
+//   letterSpacing: 0,
+// };
 
 export const bodyLong02 = {
   fontSize: rem(scale[2]),
@@ -78,17 +78,17 @@ export const bodyLong02 = {
   letterSpacing: 0,
 };
 
-export const code01 = {
-  fontFamily: fontFamilies.mono,
-  fontSize: rem(scale[0]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.34,
-  letterSpacing: px(0.32),
-};
+// export const code01 = {
+//   fontFamily: fontFamilies.mono,
+//   fontSize: rem(scale[0]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.34,
+//   letterSpacing: px(0.32),
+// };
 
 export const code02 = {
   fontFamily: fontFamilies.mono,
-  fontSize: rem(scale[1]),
+  fontSize: rem(scale[2]),
   fontWeight: fontWeights.regular,
   lineHeight: 1.43,
   letterSpacing: px(0.32),
@@ -101,8 +101,6 @@ export const heading01 = {
   letterSpacing: px(0.16),
 };
 
-export const productiveHeading01 = heading01;
-
 export const heading02 = {
   fontSize: rem(scale[2]),
   fontWeight: fontWeights.semibold,
@@ -110,25 +108,31 @@ export const heading02 = {
   letterSpacing: 0,
 };
 
-export const productiveHeading02 = heading02;
+// customize productiveHeading01 to be prev heading01
+export const productiveHeading01 = heading02;
+
+export const productiveHeading02 = {
+  ...heading02,
+  fontSize: rem(scale[3]),
+};
 
 export const productiveHeading03 = {
   fontSize: rem(scale[4]),
-  fontWeight: fontWeights.regular,
+  fontWeight: fontWeights.semibold,
   lineHeight: 1.4,
   letterSpacing: 0,
 };
 
 export const productiveHeading04 = {
   fontSize: rem(scale[6]),
-  fontWeight: fontWeights.regular,
+  fontWeight: fontWeights.semibold,
   lineHeight: 1.29,
   letterSpacing: 0,
 };
 
 export const productiveHeading05 = {
-  fontSize: rem(scale[7]),
-  fontWeight: fontWeights.regular,
+  fontSize: rem(scale[5]),
+  fontWeight: fontWeights.semibold,
   lineHeight: 1.25,
   letterSpacing: 0,
 };
@@ -140,136 +144,136 @@ export const productiveHeading06 = {
   letterSpacing: 0,
 };
 
-export const productiveHeading07 = {
-  fontSize: rem(scale[11]),
-  fontWeight: fontWeights.light,
-  lineHeight: 1.19,
-  letterSpacing: 0,
-};
+// export const productiveHeading07 = {
+//   fontSize: rem(scale[11]),
+//   fontWeight: fontWeights.light,
+//   lineHeight: 1.19,
+//   letterSpacing: 0,
+// };
 
-export const expressiveHeading01 = {
-  ...heading01,
-  lineHeight: 1.25,
-};
+// export const expressiveHeading01 = {
+//   ...heading01,
+//   lineHeight: 1.25,
+// };
 
-export const expressiveHeading02 = {
-  ...heading02,
-  lineHeight: 1.5,
-};
+// export const expressiveHeading02 = {
+//   ...heading02,
+//   lineHeight: 1.5,
+// };
 
-export const expressiveHeading03 = {
-  fontSize: rem(scale[4]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.4,
-  letterSpacing: 0,
-  breakpoints: {
-    xlg: {
-      fontSize: rem(scale[4]),
-      lineHeight: 1.25,
-    },
-    max: {
-      fontSize: rem(scale[5]),
-      lineHeight: 1.334,
-    },
-  },
-};
+// export const expressiveHeading03 = {
+//   fontSize: rem(scale[4]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.4,
+//   letterSpacing: 0,
+//   breakpoints: {
+//     xlg: {
+//       fontSize: rem(scale[4]),
+//       lineHeight: 1.25,
+//     },
+//     max: {
+//       fontSize: rem(scale[5]),
+//       lineHeight: 1.334,
+//     },
+//   },
+// };
 
-export const expressiveHeading04 = {
-  fontSize: rem(scale[6]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.29,
-  letterSpacing: 0,
-  breakpoints: {
-    xlg: {
-      fontSize: rem(scale[6]),
-      lineHeight: 1.25,
-    },
-    max: {
-      fontSize: rem(scale[7]),
-    },
-  },
-};
+// export const expressiveHeading04 = {
+//   fontSize: rem(scale[6]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.29,
+//   letterSpacing: 0,
+//   breakpoints: {
+//     xlg: {
+//       fontSize: rem(scale[6]),
+//       lineHeight: 1.25,
+//     },
+//     max: {
+//       fontSize: rem(scale[7]),
+//     },
+//   },
+// };
 
-export const expressiveHeading05 = {
-  fontSize: rem(scale[7]),
-  fontWeight: fontWeights.regular,
-  lineHeight: 1.25,
-  letterSpacing: 0,
-  breakpoints: {
-    md: {
-      fontSize: rem(scale[8]),
-      fontWeight: fontWeights.light,
-      lineHeight: 1.22,
-      letterSpacing: 0,
-    },
-    lg: {
-      fontSize: rem(scale[9]),
-      fontWeight: fontWeights.light,
-      lineHeight: 1.19,
-      letterSpacing: 0,
-    },
-    xlg: {
-      fontSize: rem(scale[10]),
-      fontWeight: fontWeights.light,
-      lineHeight: 1.17,
-      letterSpacing: 0,
-    },
-    max: {
-      fontSize: rem(scale[12]),
-      fontWeight: fontWeights.light,
-      letterSpacing: 0,
-    },
-  },
-};
+// export const expressiveHeading05 = {
+//   fontSize: rem(scale[7]),
+//   fontWeight: fontWeights.regular,
+//   lineHeight: 1.25,
+//   letterSpacing: 0,
+//   breakpoints: {
+//     md: {
+//       fontSize: rem(scale[8]),
+//       fontWeight: fontWeights.light,
+//       lineHeight: 1.22,
+//       letterSpacing: 0,
+//     },
+//     lg: {
+//       fontSize: rem(scale[9]),
+//       fontWeight: fontWeights.light,
+//       lineHeight: 1.19,
+//       letterSpacing: 0,
+//     },
+//     xlg: {
+//       fontSize: rem(scale[10]),
+//       fontWeight: fontWeights.light,
+//       lineHeight: 1.17,
+//       letterSpacing: 0,
+//     },
+//     max: {
+//       fontSize: rem(scale[12]),
+//       fontWeight: fontWeights.light,
+//       letterSpacing: 0,
+//     },
+//   },
+// };
 
-export const expressiveHeading06 = {
-  fontSize: rem(scale[7]),
-  fontWeight: fontWeights.semibold,
-  lineHeight: 1.25,
-  letterSpacing: 0,
-  breakpoints: {
-    md: {
-      fontSize: rem(scale[8]),
-      fontWeight: fontWeights.semibold,
-      lineHeight: 1.22,
-      letterSpacing: 0,
-    },
-    lg: {
-      fontSize: rem(scale[9]),
-      fontWeight: fontWeights.semibold,
-      lineHeight: 1.19,
-      letterSpacing: 0,
-    },
-    xlg: {
-      fontSize: rem(scale[10]),
-      fontWeight: fontWeights.semibold,
-      lineHeight: 1.17,
-      letterSpacing: 0,
-    },
-    max: {
-      fontSize: rem(scale[12]),
-      fontWeight: fontWeights.semibold,
-      letterSpacing: 0,
-    },
-  },
-};
+// export const expressiveHeading06 = {
+//   fontSize: rem(scale[7]),
+//   fontWeight: fontWeights.semibold,
+//   lineHeight: 1.25,
+//   letterSpacing: 0,
+//   breakpoints: {
+//     md: {
+//       fontSize: rem(scale[8]),
+//       fontWeight: fontWeights.semibold,
+//       lineHeight: 1.22,
+//       letterSpacing: 0,
+//     },
+//     lg: {
+//       fontSize: rem(scale[9]),
+//       fontWeight: fontWeights.semibold,
+//       lineHeight: 1.19,
+//       letterSpacing: 0,
+//     },
+//     xlg: {
+//       fontSize: rem(scale[10]),
+//       fontWeight: fontWeights.semibold,
+//       lineHeight: 1.17,
+//       letterSpacing: 0,
+//     },
+//     max: {
+//       fontSize: rem(scale[12]),
+//       fontWeight: fontWeights.semibold,
+//       letterSpacing: 0,
+//     },
+//   },
+// };
 
-export const expressiveParagraph01 = {
-  fontSize: rem(scale[5]),
-  fontWeight: fontWeights.light,
-  lineHeight: 1.334,
-  letterSpacing: 0,
-  breakpoints: {
-    lg: {
-      fontSize: rem(scale[6]),
-      lineHeight: 1.29,
-    },
-    max: {
-      fontSize: rem(scale[7]),
-      lineHeight: 1.25,
-    },
-  },
-};
+// export const expressiveParagraph01 = {
+//   fontSize: rem(scale[5]),
+//   fontWeight: fontWeights.light,
+//   lineHeight: 1.334,
+//   letterSpacing: 0,
+//   breakpoints: {
+//     lg: {
+//       fontSize: rem(scale[6]),
+//       lineHeight: 1.29,
+//     },
+//     max: {
+//       fontSize: rem(scale[7]),
+//       lineHeight: 1.25,
+//     },
+//   },
+// };
 
 export const quotation01 = {
   fontSize: rem(scale[4]),
