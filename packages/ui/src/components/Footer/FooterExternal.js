1429c9ee2220aa42b76d3c842aba90fced2fd1c6
Alessio Rapini <alessio.rapini@yahoo.it>
2021-10-15 12:30:36 +0200

    Improve FooterExternal classnames

--- packages/ui/src/components/Footer/FooterExternal.js
+++ packages/ui/src/components/Footer/FooterExternal.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import Icon from '../Icon';
 import Link from '../Link';
 import Wrapper from '../Wrapper';
 
 import { Home16 } from '@wfp/icons-react';
-import { iconHome } from '@wfp/icons';
 import { WfpLogoVerticalEn } from '@wfp/pictograms-react';
 
 import { settings } from '../../globals/js';
@@ -16,50 +14,43 @@ const ExternalFooter = () => (
   <footer className={`${prefix}--footer-ext`}>
     <Wrapper pageWidth="lg">
       <div className={`${prefix}--footer-ext__content`}>
-        <div className={`${prefix}--footer-ext__content__corporate`}>
-          <div className={`${prefix}--footer-ext__content__corporate__logo`}>
+        <div className={`${prefix}--footer-ext__info`}>
+          <div className={`${prefix}--footer-ext__branding`}>
             <WfpLogoVerticalEn alt="WFP" />
-            <div
-              className={`${prefix}--footer-ext__content__corporate__logo__service`}>
+            <div className={`${prefix}--footer-ext__product-name`}>
               <span>
                 Product <br />
                 Name
               </span>
             </div>
           </div>
-          <div className={`${prefix}--footer-ext__content__corporate__address`}>
+          <div className={`${prefix}--footer-ext__address`}>
             Via C. G. Viola 68 Parco dei Medici, 00148 Rome, Italy
           </div>
-          <div className={`${prefix}--footer-ext__content__corporate__social`}>
+          <div className={`${prefix}--footer-ext__social-wrapper`}>
             <p>Follow WFP on:</p>
-            <ul
-              className={`${prefix}--footer-ext__content__corporate__social__list`}>
-              <li
-                className={`${prefix}--footer-ext__content__corporate__social__list__icon`}>
+            <ul className={`${prefix}--footer-ext__social-icons-list`}>
+              <li className={`${prefix}--footer-ext__social-icon`}>
                 <a>
                   <Home16 icon={Home16} height={24} width={24} />
                 </a>
               </li>
-              <li
-                className={`${prefix}--footer-ext__content__corporate__social__list__icon`}>
+              <li className={`${prefix}--footer-ext__social-icon`}>
                 <a>
                   <Home16 icon={Home16} height={24} width={24} />
                 </a>
               </li>
-              <li
-                className={`${prefix}--footer-ext__content__corporate__social__list__icon`}>
+              <li className={`${prefix}--footer-ext__social-icon`}>
                 <a>
                   <Home16 icon={Home16} height={24} width={24} />
                 </a>
               </li>
-              <li
-                className={`${prefix}--footer-ext__content__corporate__social__list__icon`}>
+              <li className={`${prefix}--footer-ext__social-icon`}>
                 <a>
                   <Home16 icon={Home16} height={24} width={24} />
                 </a>
               </li>
-              <li
-                className={`${prefix}--footer-ext__content__corporate__social__list__icon`}>
+              <li className={`${prefix}--footer-ext__social-icon`}>
                 <a>
                   <Home16 icon={Home16} height={24} width={24} />
                 </a>
@@ -67,54 +58,46 @@ const ExternalFooter = () => (
             </ul>
           </div>
         </div>
-        <div className={`${prefix}--footer-ext__content__links`}>
-          <div className={`${prefix}--footer-ext__content__links__column`}>
-            <p
-              className={`${prefix}--footer-ext__content__links__column__title`}>
-              First title
-            </p>
+        <div className={`${prefix}--footer-ext__nav-wrapper`}>
+          <div className={`${prefix}--footer-ext__nav-column`}>
+            <p className={`${prefix}--footer-ext__nav-title`}>First title</p>
             <nav>
-              <ul
-                className={`${prefix}--footer-ext__content__links__column__nav-list`}>
-                <li>
+              <ul className={`${prefix}--footer-ext__nav-list`}>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>First link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Second link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Third link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Fourth link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Fifth link</Link>
                 </li>
               </ul>
             </nav>
           </div>
-          <div className={`${prefix}--footer-ext__content__links__column`}>
-            <p
-              className={`${prefix}--footer-ext__content__links__column__title`}>
-              Second title
-            </p>
+          <div className={`${prefix}--footer-ext__nav-column`}>
+            <p className={`${prefix}--footer-ext__nav-title`}>Second title</p>
             <nav>
-              <ul
-                className={`${prefix}--footer-ext__content__links__column__nav-list`}>
-                <li>
+              <ul className={`${prefix}--footer-ext__nav-list`}>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>First link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Second link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Third link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Fourth link</Link>
                 </li>
-                <li>
+                <li className={`${prefix}--footer-ext__nav-link`}>
                   <Link>Fifth link</Link>
                 </li>
               </ul>
@@ -124,12 +107,12 @@ const ExternalFooter = () => (
       </div>
       <div className={`${prefix}--footer-ext__legal`}>
         <span>{new Date().getFullYear()} © World Food Programme</span>
-        <nav className={`${prefix}--footer-ext__legal__nav`}>
-          <ul className={`${prefix}--footer-ext__legal__nav__links`}>
-            <li className={`${prefix}--footer-ext__legal__nav__links__link`}>
+        <nav className={`${prefix}--footer-ext__nav-legal`}>
+          <ul className={`${prefix}--footer-ext__legal-links`}>
+            <li className={`${prefix}--footer-ext__legal-link`}>
               <Link>First legal link</Link>
             </li>
-            <li className={`${prefix}--footer-ext__legal__nav__links__link`}>
+            <li className={`${prefix}--footer-ext__legal-link`}>
               <Link>Second legal link</Link>
             </li>
           </ul>
