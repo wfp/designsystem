d4ae533824a676a7d0e2a1943de3e881deae2b5f
Robert Gühne <robert@polyxostudios.de>
2021-11-11 19:21:07 +0100

    fix(ui): removed legacy copy-vendor-styles

--- packages/ui/scripts/env.js
+++ packages/ui/scripts/env.js
@@ -2,16 +2,32 @@
 
 const BABEL_ENV = process.env.BABEL_ENV;
 
+const docgenConfig = {
+  plugins: [
+    [
+      'babel-plugin-react-docgen',
+      {
+        removeMethods: true,
+      },
+    ],
+  ],
+};
+
 module.exports = () => ({
+  plugins:
+    BABEL_ENV === 'es' || BABEL_ENV === 'cjs'
+      ? ['@babel/plugin-transform-runtime']
+      : undefined,
   presets: [
     [
-      '@babel/preset-env',
+      require.resolve('@babel/preset-env'),
       {
         modules: BABEL_ENV === 'es' ? false : 'commonjs',
         targets: {
-          browsers: ['last 1 versions', 'Firefox ESR'],
+          browsers: ['extends browserslist-config-carbon'],
         },
       },
     ],
   ],
+  ...(BABEL_ENV === 'docgen' && docgenConfig),
 });
