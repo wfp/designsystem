95dc1621cd50352aad38c11f26071b288cad9b7d
cassandra <cassandra.kesewa@gmail.com>
2022-03-31 11:37:32 +0000

    fix(NumberInput): NumberInput ttype definition covering both number and string for step prop #513

--- src/types/NumberInput.d.ts
+++ src/types/NumberInput.d.ts
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { InputProps } from './Input'
+import { InputProps } from './Input';
 
-declare namespace NumberInput  {
+declare namespace NumberInput {
   interface NumberInputProps extends InputProps {
-    max?: number
-    min?: number
-    step?: number
-    allowEmpty?: boolean
+    max?: number;
+    min?: number;
+    step?: number | string;
+    allowEmpty?: boolean;
   }
 }
 
-declare class NumberInput extends React.Component<NumberInput.NumberInputProps>{ }
-export = NumberInput
+declare class NumberInput extends React.Component<NumberInput.NumberInputProps> {}
+export = NumberInput;
