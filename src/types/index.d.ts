8478b65bf0eb75b589ae39d85d4300cb8e0391ae
Robert <robert@wirewire.de>
2022-08-03 17:24:19 +0200

    fix(StepNavigation): propTypes added and fixed

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,107 +1,107 @@
-import AnchorNavigation = require('./AnchorNavigation')
-import { AnchorNavigationProps } from './AnchorNavigation'
-import AuthLayout = require('./AuthLayout')
-import { AuthLayoutProps } from './AuthLayout'
-import BackgroundContent = require('./BackgroundContent')
-import { BackgroundContentProps } from './BackgroundContent'
-import BannerNavigation = require('./BannerNavigation')
-import { BannerNavigationProps } from './BannerNavigation'
-import BannerNavigationItem = require('./BannerNavigationItem')
-import { BannerNavigationItemProps } from './BannerNavigationItem'
-import Blockquote = require('./Blockquote')
-import { BlockquoteProps } from './Blockquote'
-import Breadcrumb = require('./Breadcrumb')
-import { BreadcrumbProps } from './Breadcrumb'
-import BreadcrumbHome = require('./BreadcrumbHome')
-import { BreadcrumbHomeProps } from './BreadcrumbHome'
-import BreadcrumbItem = require('./BreadcrumbItem')
-import { BreadcrumbItemProps } from './BreadcrumbItem'
-import Button = require('./Button')
-import { ButtonProps } from './Button'
-import Card = require('./Card')
-import { CardProps } from './Card'
-import Checkbox = require('./Checkbox')
-import { CheckboxProps } from './Checkbox'
-import InputGroup = require('./InputGroup')
-import { InputGroupProps } from './InputGroup'
-import ContentSwitcher = require('./ContentSwitcher')
-import { ContentSwitcherProps } from './ContentSwitcher'
-import ContextMenu = require('./ContextMenu')
-import { ContextMenuProps } from './ContextMenu'
-import ContextMenuGroup = require('./ContextMenuGroup')
-import { ContextMenuGroupProps } from './ContextMenuGroup'
-import ContextMenuItem = require('./ContextMenuItem')
-import { ContextMenuItemProps } from './ContextMenuItem'
-import Credits = require('./Credits')
-import { CreditsProps } from './Credits'
-import DateRangePickerInput = require('./DateRangePickerInput')
-import { DateRangePickerInputProps } from './DateRangePickerInput'
-import SingleDatePickerInput = require('./SingleDatePickerInput')
-import { SingleDatePickerInputProps } from './SingleDatePickerInput'
-import Empty = require('./Empty')
-import { EmptyProps } from './Empty'
-import Footer = require('./Footer')
-import { FooterProps } from './Footer'
-import Form = require('./Form')
-import { FormProps } from './Form'
-import FormGroup = require('./FormGroup')
-import { FormGroupProps } from './FormGroup'
-import FormHint = require('./FormHint')
-import { FormHintProps } from './FormHint'
-import FormLabel = require('./FormLabel')
-import { FormLabelProps } from './FormLabel'
-import FormWizard = require('./FormWizard')
-import { FormWizardProps } from './FormWizard'
-import Hero = require('./Hero')
-import { HeroProps } from './Hero'
-import Icon = require('./Icon')
-import { IconProps } from './Icon'
-import InfoBar = require('./InfoBar')
-import { InfoBarProps } from './InfoBar'
-import InlineLoading = require('./InlineLoading')
-import { InlineLoadingProps } from './InlineLoading'
-import Input = require('./Input')
-import { InputProps } from './Input'
-import Item = require('./Item')
-import { ItemProps } from './Item'
-import Link = require('./Link')
-import { LinkProps } from './Link'
-import Loading = require('./Loading')
-import { LoadingProps } from './Loading'
-import NumberInput = require('./NumberInput')
-import { NumberInputProps } from './NumberInput'
-import Pagination = require('./Pagination')
-import { PaginationProps } from './Pagination'
-import RadioButton = require('./RadioButton')
-import { RadioButtonProps } from './RadioButton'
-import RadioButtonGroup = require('./RadioButtonGroup')
-import { RadioButtonGroupProps } from './RadioButtonGroup'
-import ReadMore = require('./ReadMore')
-import { ReadMoreProps } from './ReadMore'
-import Search = require('./Search')
-import { SearchProps } from './Search'
-import Story = require('./Story')
-import { StoryProps } from './Story'
-import Tag = require('./Tag')
-import { TagProps } from './Tag'
-import Text = require('./Text')
-import { TextProps } from './Text'
-import TextArea = require('./TextArea')
-import { TextAreaProps } from './TextArea'
-import TextInput = require('./TextInput')
-import { TextInputProps } from './TextInput'
-import Toggle = require('./Toggle')
-import { ToggleProps } from './Toggle'
-import Tooltip = require('./Tooltip')
-import { TooltipProps } from './Tooltip'
-import Unit = require('./Unit')
-import { UnitProps } from './Unit'
-import User = require('./User')
-import { UserProps } from './User'
-import Value = require('./Value')
-import { ValueProps } from './Value'
-import Wrapper = require('./Wrapper')
-import { WrapperProps } from './Wrapper'
+import AnchorNavigation = require('./AnchorNavigation');
+import { value AnchorNavigationProps } from './AnchorNavigation';
+import AuthLayout = require('./AuthLayout');
+import { value AuthLayoutProps } from './AuthLayout';
+import BackgroundContent = require('./BackgroundContent');
+import { value BackgroundContentProps } from './BackgroundContent';
+import BannerNavigation = require('./BannerNavigation');
+import { value BannerNavigationProps } from './BannerNavigation';
+import BannerNavigationItem = require('./BannerNavigationItem');
+import { value BannerNavigationItemProps } from './BannerNavigationItem';
+import Blockquote = require('./Blockquote');
+import { value BlockquoteProps } from './Blockquote';
+import Breadcrumb = require('./Breadcrumb');
+import { value BreadcrumbProps } from './Breadcrumb';
+import BreadcrumbHome = require('./BreadcrumbHome');
+import { value BreadcrumbHomeProps } from './BreadcrumbHome';
+import BreadcrumbItem = require('./BreadcrumbItem');
+import { value BreadcrumbItemProps } from './BreadcrumbItem';
+import Button = require('./Button');
+import { value ButtonProps } from './Button';
+import Card = require('./Card');
+import { value CardProps } from './Card';
+import Checkbox = require('./Checkbox');
+import { value CheckboxProps } from './Checkbox';
+import InputGroup = require('./InputGroup');
+import { value InputGroupProps } from './InputGroup';
+import ContentSwitcher = require('./ContentSwitcher');
+import { value ContentSwitcherProps } from './ContentSwitcher';
+import ContextMenu = require('./ContextMenu');
+import { value ContextMenuProps } from './ContextMenu';
+import ContextMenuGroup = require('./ContextMenuGroup');
+import { value ContextMenuGroupProps } from './ContextMenuGroup';
+import ContextMenuItem = require('./ContextMenuItem');
+import { value ContextMenuItemProps } from './ContextMenuItem';
+import Credits = require('./Credits');
+import { value CreditsProps } from './Credits';
+import DateRangePickerInput = require('./DateRangePickerInput');
+import { value DateRangePickerInputProps } from './DateRangePickerInput';
+import SingleDatePickerInput = require('./SingleDatePickerInput');
+import { value SingleDatePickerInputProps } from './SingleDatePickerInput';
+import Empty = require('./Empty');
+import { value EmptyProps } from './Empty';
+import Footer = require('./Footer');
+import { value FooterProps } from './Footer';
+import Form = require('./Form');
+import { value FormProps } from './Form';
+import FormGroup = require('./FormGroup');
+import { value FormGroupProps } from './FormGroup';
+import FormHint = require('./FormHint');
+import { value FormHintProps } from './FormHint';
+import FormLabel = require('./FormLabel');
+import { value FormLabelProps } from './FormLabel';
+import FormWizard = require('./FormWizard');
+import { value FormWizardProps } from './FormWizard';
+import Hero = require('./Hero');
+import { value HeroProps } from './Hero';
+import Icon = require('./Icon');
+import { value IconProps } from './Icon';
+import InfoBar = require('./InfoBar');
+import { value InfoBarProps } from './InfoBar';
+import InlineLoading = require('./InlineLoading');
+import { value InlineLoadingProps } from './InlineLoading';
+import Input = require('./Input');
+import { value InputProps } from './Input';
+import Item = require('./Item');
+import { value ItemProps } from './Item';
+import Link = require('./Link');
+import { value LinkProps } from './Link';
+import Loading = require('./Loading');
+import { value LoadingProps } from './Loading';
+import NumberInput = require('./NumberInput');
+import { value NumberInputProps } from './NumberInput';
+import Pagination = require('./Pagination');
+import { value PaginationProps } from './Pagination';
+import RadioButton = require('./RadioButton');
+import { value RadioButtonProps } from './RadioButton';
+import RadioButtonGroup = require('./RadioButtonGroup');
+import { value RadioButtonGroupProps } from './RadioButtonGroup';
+import ReadMore = require('./ReadMore');
+import { value ReadMoreProps } from './ReadMore';
+import Search = require('./Search');
+import { value SearchProps } from './Search';
+import Story = require('./Story');
+import { value StoryProps } from './Story';
+import Tag = require('./Tag');
+import { value TagProps } from './Tag';
+import Text = require('./Text');
+import { value TextProps } from './Text';
+import TextArea = require('./TextArea');
+import { value TextAreaProps } from './TextArea';
+import TextInput = require('./TextInput');
+import { value TextInputProps } from './TextInput';
+import Toggle = require('./Toggle');
+import { value ToggleProps } from './Toggle';
+import Tooltip = require('./Tooltip');
+import { value TooltipProps } from './Tooltip';
+import Unit = require('./Unit');
+import { value UnitProps } from './Unit';
+import User = require('./User');
+import { value UserProps } from './User';
+import Value = require('./Value');
+import { value ValueProps } from './Value';
+import Wrapper = require('./Wrapper');
+import { value WrapperProps } from './Wrapper';
 
 export {
   AnchorNavigation,
@@ -207,15 +207,15 @@ export {
   Value,
   ValueProps,
   Wrapper,
-  WrapperProps
-}
+  WrapperProps,
+};
 
-export * from './List'
-export * from './Navigation'
-export * from './Modal'
-export * from './Module'
-export * from './Notification'
-export * from './Select'
-export * from './Sidebar'
-export * from './Tabs'
-export * from './Table'
+export * from './List';
+export * from './Navigation';
+export * from './Modal';
+export * from './Module';
+export * from './Notification';
+export * from './Select';
+export * from './Sidebar';
+export * from './Tabs';
+export * from './Table';
