18553546893d1d5e064d52f6f9ee85e8ea6bd3e4
Robert Gühne <robert@wirewire.de>
2022-09-19 11:10:32 +0200

    feat(website): styles and favicon

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -1,4 +1,4 @@
-import '../style.scss';
+import '../scss/style.scss';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from '@fortawesome/fontawesome-svg-core';
