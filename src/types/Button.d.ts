9e878a99deb33052860b914e460c5788842e4c28
Norbert Songin <norbert.songin@tivix.com>
2023-03-03 16:14:41 +0100

    fix: split ButtonProps interface into button and link variants

--- src/types/Button.d.ts
+++ src/types/Button.d.ts
@@ -2,17 +2,26 @@ import * as React from 'react';
 import { ButtonKind, IIcon } from './utils';
 
 declare namespace Button {
-  interface ButtonProps extends React.ButtonHTMLAttributes<Button> {
+  interface ButtonBaseProps  {
     children?: React.ReactNode;
     small?: boolean;
     large?: boolean;
     kind?: ButtonKind;
-    href?: string;
     icon?: IIcon;
     iconReverse?: boolean;
     // todo: revise iconDescription
     iconDescription?: string;
   }
+
+  interface ButtonButtonProps extends ButtonBaseProps, React.ButtonHTMLAttributes<Button> {
+  }
+
+  interface ButtonLinkProps extends ButtonBaseProps, React.LinkHTMLAttributes<Button> {
+    href?: string;
+    target?: string;
+  }
+
+  type ButtonProps = ButtonButtonProps | ButtonLinkProps
 }
 
 declare class Button extends React.Component<Button.ButtonProps> {}
